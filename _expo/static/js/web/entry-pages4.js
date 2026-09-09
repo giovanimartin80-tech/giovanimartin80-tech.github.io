c23d7d4873b9012af7bc8f091c6809406c3e4f38
giovanimartin80-tech <giovani.martin80@gmail.com>
2026-09-09 09:01:51 -1000

    Add pages4 concatenating boot loader.

--- _expo/static/js/web/entry-pages4.js
+++ _expo/static/js/web/entry-pages4.js
@@ -1,6 +1,7 @@
 (function(){
- var N=170;
- function pad(i){i=String(i);while(i.length<3)i='0'+i;return i;}
+ var N=85;
+ var HEX={1:28};
+ function pad(i,w){i=String(i);while(i.length<w)i='0'+i;return i;}
  function unhex(s){
   s=s.replace(/\s+/g,'');
   var a=new Uint8Array(s.length/2);
@@ -11,13 +12,24 @@
   document.body.innerHTML='<p style="font-family:Raleway,sans-serif;color:#fff;padding:24px;background:#000">Freedom Formula preview failed to load. Refresh once.</p>';
   console.error(e);
  }
- Promise.all(Array.from({length:N},function(_,i){
-  return fetch('/_expo/static/js/web/h4/'+pad(i)+'.hex?v=pages4').then(function(r){
-   if(!r.ok) throw new Error('hex '+i+' '+r.status);
-   return r.text();
-  });
- })).then(function(xs){
-  var parts=xs.map(unhex);
+ function loadOne(i){
+  if(HEX[i]){
+   var n=HEX[i], parts=[];
+   function next(j){
+    if(j>=n) return Promise.resolve(unhex(parts.join('')));
+    return fetch('/_expo/static/js/web/c4/'+pad(i,3)+'.js.hex.'+pad(j,2)+'?v=pages4').then(function(r){
+     if(!r.ok) throw new Error('hex '+i+' '+j+' '+r.status);
+     return r.text();
+    }).then(function(t){ parts.push(t); return next(j+1); });
+   }
+   return next(0);
+  }
+  return fetch('/_expo/static/js/web/c4/'+pad(i,3)+'.js?v=pages4').then(function(r){
+   if(!r.ok) throw new Error('js '+i+' '+r.status);
+   return r.arrayBuffer();
+  }).then(function(b){ return new Uint8Array(b); });
+ }
+ Promise.all(Array.from({length:N},function(_,i){ return loadOne(i); })).then(function(parts){
   var total=0; for(var i=0;i<parts.length;i++) total+=parts[i].length;
   var out=new Uint8Array(total), o=0;
   for(var i=0;i<parts.length;i++){ out.set(parts[i], o); o+=parts[i].length; }
